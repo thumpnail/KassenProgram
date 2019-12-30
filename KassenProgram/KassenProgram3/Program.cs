3257483693e5a2c144438db372769e5db581174a
thumbnail <friedewald24@gmail.com>
2019-12-30 11:34:15 +0100

    no message

---
+++ KassenProgram/KassenProgram3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using KassenProgram;
+using KassenProgram3.Utils;
+using KassenProgram3;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KassenProgram {
+    public class Program {
+        public static MainForm form = new MainForm();
+        public static void Main(string[] args) {
+            Console.Clear();
+
+            string JSNDBFile = Directory.GetCurrentDirectory() + @"\products.json";
+            Console.WriteLine(JSNDBFile);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            jsonReaderWriter.ReadJSON(JSNDBFile);
+
+            ProductDB.AddProduct(1, "choclate", "Schokolade", 5, 6, 7, 10.05, "alle lieben fleisch", 19, DateTime.Now);
+            //ProductDB.AddProduct(2,"coffe","Kaffe",2,3,4,12.99,"Kaffe ist immer gut", 14, DateTime.Now);
+            //ProductDB.AddProduct(3,"tea","Schwarz Tee",5,6,7,11.20,"Schwarzer tee ist besser als Kaffe", 14, DateTime.UtcNow);
+
+            StartGUI();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            jsonReaderWriter.WriteJSON(JSNDBFile);
+        }
+        [STAThread]
+        public static void StartGUI() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(form);
+        }
+    }
+}
+
